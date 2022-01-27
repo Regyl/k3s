f9096dc303b0391ab5cac1e5d0d024558ed7a1b4
Regyl <novikovevgeny1@gmail.com>
2022-01-27 06:18:54 -0500

    add loki.sh

---
+++ kuber-images/grafana/loki/loki.sh
@@ -0,0 +1,6 @@
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+pid=$!
+wait $pid
+helm upgrade --install loki  grafana/loki -f loki.yaml
+echo "Go to Grafana and put the next Loki's URL: http://loki:3100/"
