c1d1f682b8771c2e004a5d74bab13710abc09e3e
Regyl <novikovevgeny1@gmail.com>
2022-01-27 07:32:14 -0500

    chmod for each and all

--- kuber-images/postgres/postgres.sh
+++ kuber-images/postgres/postgres.sh
@@ -1,7 +1,7 @@
 k3s kubectl create -f postgres-configmap.yaml
 sleep 2 
 k3s kubectl create -f postgres-storage.yaml
-slep 2 
+sleep 2 
 k3s kubectl create -f postgres-deployment.yaml
 sleep 2 
 k3s kubectl create -f postgres-service.yaml
