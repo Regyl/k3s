b078a84845cc9f29f00ec7bd38f0e1552969778c
Regyl <novikovevgeny1@gmail.com>
2022-01-27 06:11:52 -0500

    update grafana.sh

--- kuber-images/grafana/grafana.sh
+++ kuber-images/grafana/grafana.sh
@@ -1,4 +1,4 @@
 k3s kubectl apply -f grafana.yaml
-echo "Wait around 20 seconds and run the command below:"
-echo "k3s kubectl port-forward service/grafana 3000:3000"
+sleep 20
+k3s kubectl port-forward service/grafana 3000:3000
 echo "Go to the <IPv4>:3000. Login: admin, password: admin"
