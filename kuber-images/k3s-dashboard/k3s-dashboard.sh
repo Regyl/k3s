20dd226a499f770785051b57765ec3239f9d08c1
Regyl <novikovevgeny1@gmail.com>
2022-01-27 06:23:30 -0500

    update k3s-dashboard.sh

--- kuber-images/k3s-dashboard/k3s-dashboard.sh
+++ kuber-images/k3s-dashboard/k3s-dashboard.sh
@@ -1,3 +1,8 @@
 GITHUB_URL=https://github.com/kubernetes/dashboard/releases
 VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
 k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+pid=$!
+wait $pid
+k3s kubectl create -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
+echo "Token for dashboard auth:"
+k3s kubectl -n kubernetes-dashboard describe secret admin-user-token | grep '^token'
