098bbbb897fd379457634fee107854a411941366
Regyl <novikovevgeny1@gmail.com>
2022-01-27 07:29:44 -0500

    add postgres.sh

---
+++ kuber-images/postgres/postgres.sh
@@ -0,0 +1,8 @@
+k3s kubectl create -f postgres-configmap.yaml
+sleep 2 
+k3s kubectl create -f postgres-storage.yaml
+slep 2 
+k3s kubectl create -f postgres-deployment.yaml
+sleep 2 
+k3s kubectl create -f postgres-service.yaml
+sleep 2
