d1c753103ac0ef9a895744447c26f02f4cd2ef62
regyl <corgidile01@gmail.com>
2023-07-28 18:30:02 -0400

    feature: update spring boot template

--- k3s.sh
+++ k3s.sh
@@ -1,13 +1,12 @@
-#echo "1. Install clear k3s"
-#curl -sfL https://get.k3s.io | sh -
-#sleep 30
-#k3s kubectl get all
+echo "1. Install clear k3s"
+curl -sfL https://get.k3s.io | sh -
+sleep 30
+k3s kubectl get all
 
 echo "2. Install k3s-dashboard"
 ./kuber-images/k3s-dashboard/k3s-dashboard.sh
 pid=$!
 wait $pid
-exit 2
 
 echo "3. Install Grafana"
 ./kuber-images/grafana/grafana.sh
