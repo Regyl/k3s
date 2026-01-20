d23735b06a142a0f7c1e9652b67f743ce8b1d60a
novik <novikovevgeny1@gmail.com>
2026-01-20 19:05:52 +0300

    feat: add CentOS  9, remove .idea dir

---
+++ centos9/quick-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "1. Update installed packages"
+dnf update
+
+echo "2. Let's check SSH default port (it must differs from 22)"
+telnet_output="$({ sleep 1; echo $'\e'; } | telnet localhost 22 2>&1)"
+if [[ "$telnet_output" == *"Connection refused"* ]];
+	then echo "Success: 22 port connection refused"
+else
+	echo "Result: fail. Connection success. Change default SSH port"
+	exit 1
+fi
